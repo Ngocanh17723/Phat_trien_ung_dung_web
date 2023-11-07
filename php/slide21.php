9d554db3145bce2a82f39dce0c27adc3cde26ae1
Ngocanh17723 <ayo1ng2k3@gmail.com>
2023-11-07 19:10:39 +0700

    sldie21

---
+++ php/slide21.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir ="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <?php
+    echo 'MY FIRST PHP FILE';
+    ?>
+    <h3>Hello Word!</h3>
+    <select>1</select>
+    <select>2</select>
+</select>
+</body>
+</html>
