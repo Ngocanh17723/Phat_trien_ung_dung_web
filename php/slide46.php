d60e59d001a23efa6cdaefb4bb1ef6d08e762e3a
Ngocanh17723 <ayo1ng2k3@gmail.com>
2023-11-08 15:15:49 +0700

    slide46

---
+++ php/slide46.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir ="ltr">
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+<form action ="7-CalResult.php"method = "get">
+        <p>First Number:</p>
+        <input type="number"Name ="First Number"><br>
+        <p>Second Number:</p>
+        <input type="number" name ="Second Number"><br>
+        <p></p>
+        <input type="submit" value ="Add">
+    </form>
+</body>
+</html>
