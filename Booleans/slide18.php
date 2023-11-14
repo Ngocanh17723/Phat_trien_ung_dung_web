3473f178d18eb5c0588a59749e4746e69e765600
Ngocanh17723 <ayo1ng2k3@gmail.com>
2023-11-14 18:53:01 +0700

    slide18

---
+++ Booleans/slide18.php
@@ -0,0 +1,20 @@
+<?php
+$USD = 22300;
+$EUR = 27300;
+$SGD = 17000;
+$JPY = 120;
+$amount = $_GET['$amount'];
+echo "$amount USD is equal";
+if ($_GET["currency"] == "USD") {
+    echo $amount * $USD;
+}
+if ($_GET["currency"] == "EUR") {
+    echo $amount * $EUR;
+}
+if ($_GET["currency"] == "SGD") {
+    echo $amount * $SGD;
+} else {
+    echo $amount * $JPY;
+}
+echo "VND";
+?>
