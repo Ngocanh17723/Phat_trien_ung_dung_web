31402b0876cd2579f7d82fd8415b68b1e4d1f5b7
Ngocanh17723 <ayo1ng2k3@gmail.com>
2023-11-14 18:31:51 +0700

    slide2

--- Booleans/slide2.php
+++ Booleans/slide2.php
@@ -9,20 +9,20 @@
 
 <body>
     <?php
-$T = True;
-$F = False;
-echo "T = $T<br>";
-echo "F = SF<br>";
-$check1 = 10 > 5;
-$check2 = 20 < 10;
-$check3 = $check1 && $check2;
-$check4 = $check1 || $check2;
-echo "check1 = $check1 <br>";
-echo "check2 = $check2 <br>";
-echo "check3 = $check3 <br>";
-echo "check4 = $check1 ";
+    $T = True;
+    $F = False;
+    echo "T = $T<br>";
+    echo "F = SF<br>";
+    $check1 = 10 > 5;
+    $check2 = 20 < 10;
+    $check3 = $check1 && $check2;
+    $check4 = $check1 || $check2;
+    echo "check1 = $check1 <br>";
+    echo "check2 = $check2 <br>";
+    echo "check3 = $check3 <br>";
+    echo "check4 = $check1 ";
 
-?>
+    ?>
 </body>
 
 </html>
