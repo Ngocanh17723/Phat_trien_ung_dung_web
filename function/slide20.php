ffa1ea263cfaa96df7271bb62ff7acab73aaff16
Ngocanh17723 <ayo1ng2k3@gmail.com>
2023-11-28 18:40:26 +0700

    slide20

---
+++ function/slide20.php
@@ -0,0 +1,13 @@
+<?php
+$items = array('items1.png', 'items2.png');
+foreach ($items as $item) {
+    echo "<img src = 'images/$item'with=50px height =50px alt = '$item'>";
+}
+?>
+<h1>function</h1>
+<?php
+$items = array('items1.png', 'items2.png');
+foreach ($items as $item) {
+    echo "<img src = 'images/$item'with=50px height =50px alt = '$item'>";
+}
+?>
